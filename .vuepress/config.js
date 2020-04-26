b05b78ee1aa5247949fffbeefb24b6568ae8b28a
Moobicar <57834313+lucaskleiner@users.noreply.github.com>
2020-04-25 21:05:04 -0300

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -5,10 +5,10 @@ module.exports = {
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { property: 'og:type', content: 'website' }],
-    ['meta', { property: 'og:url', content: 'https://vittominacori.github.io/erc20-generator' }],
-    ['meta', { property: 'og:image', content: 'https://vittominacori.github.io/erc20-generator/assets/images/erc20-token-generator.png' }], // eslint-disable-line max-len
+    ['meta', { property: 'og:url', content: 'https://lucaskleiner.github.io/erc20-generator' }],
+    ['meta', { property: 'og:image', content: 'https://lucaskleiner.github.io/erc20-generator/assets/images/erc20-token-generator.png' }], // eslint-disable-line max-len
     ['meta', { property: 'twitter:card', content: 'summary_large_image' }],
-    ['meta', { property: 'twitter:image', content: 'https://vittominacori.github.io/erc20-generator/assets/images/erc20-token-generator.png' }], // eslint-disable-line max-len
+    ['meta', { property: 'twitter:image', content: 'https://lucaskleiner.github.io/erc20-generator/assets/images/erc20-token-generator.png' }], // eslint-disable-line max-len
     ['meta', { property: 'twitter:title', content: 'ERC20 Token Generator | Create your Token for FREE' }],
     ['script', { src: 'assets/js/web3.min.js' }],
   ],
